0efb73f5cede6b2d1fa78542389293c1c9b7725a
remorses <beats.by.morse@gmail.com>
2019-04-08 21:59:26 +0200

    make dir if not exists

--- tests/__main__.py
+++ tests/__main__.py
@@ -3,9 +3,9 @@ from video_cut import cut_timeframes, concat_videos
 timeframes = [(0, 1), (2, 3), (1, 8)]
 
 paths = cut_timeframes(
-    'tests/video.m2ts',
+    'tests/video_.m2ts',
     timeframes,
-    out_dir='./',
+    out_dir='./subclips',
     out_pattern='{name}_{i}{ext}',
 )
 
